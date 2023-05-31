87c2dac2c3690ed01e7b4208ed0289c862f80ce1
Julien QUENTIN <jqntn88@gmail.com>
2023-06-01 00:07:01 +0200

    add hello

--- quos/src/boot/boot.asm
+++ quos/src/boot/boot.asm
@@ -1,7 +1,15 @@
-; Infinite loop (e9 fd ff)
-loop:
-    jmp loop
-; Fill with 510 zeros minus the size of the previous code
+mov ah, 0x0E
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $
+
 times 510-($-$$) db 0
-; Magic number
-dw 0xaa55
+dw 0xAA55
