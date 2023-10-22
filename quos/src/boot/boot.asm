7bf6915363e1ff2bfa09eb9b302e16d6374838e1
Julien QUENTIN <jqntn88@gmail.com>
2023-10-22 18:37:13 +0200

    update

--- quos/src/boot/boot.asm
+++ quos/src/boot/boot.asm
@@ -1,15 +1,13 @@
+[org 0x7C00]
 mov ah, 0x0E
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10
-mov al, 'o'
+
+mov al, [str]
 int 0x10
 
 jmp $
+
+str:
+	db "X"
 
 times 510-($-$$) db 0
 dw 0xAA55
